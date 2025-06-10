a16767d19fdfa913659d489e9fd8b3ebfabb9f90
KingKiarie <kiarie7mbugua@gmail.com>
2025-06-10 23:16:50 +0300

    chore: updated get-users

--- lib/types/user.ts
+++ lib/types/user.ts
@@ -1,16 +1,15 @@
 export interface User {
   id: number;
-  username: string;
-  fullName: string;
   email: string;
   role: string;
   companyCode: string;
   isActive: boolean;
   createdAt: string;
+  erpUserCode?: string;
+  SalesRepName: string;
 }
 
 export interface GetUsersResponse {
-   
   data: User[];
   total: number;
 }
